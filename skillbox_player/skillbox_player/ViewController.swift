4ed4d1c5311ac0ace2cfa371054f84ddb76fa02e
Vlad <delamole09@gmail.com>
2020-07-06 20:34:18 +0300

    create new project

---
+++ skillbox_player/skillbox_player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  skillbox_player
+//
+//  Created by Владислав Бочаров on 06.07.2020.
+//  Copyright © 2020 Vladislav Bocharov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
